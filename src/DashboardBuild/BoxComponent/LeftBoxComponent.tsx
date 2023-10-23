27f1f22f240c42d377f6a7b3222d08473b52f96c
Bruno Cicerchia <brunocicerchia.dev@gmail.com>
2023-10-23 10:44:31 -0300

    DolarComponent divisa selection added

--- src/DashboardBuild/BoxComponent/LeftBoxComponent.tsx
+++ src/DashboardBuild/BoxComponent/LeftBoxComponent.tsx
@@ -5,9 +5,7 @@ export default function LeftBoxComponent() {
   return (
     <div className="row mt-md-5 mt-2">
       <div className="col-md-4">
-        <div className={styles.container}>
           <DolarBlueComponent></DolarBlueComponent>
-        </div>
       </div>
       <div className="col-md-4">
         <div className={styles.container}></div>
--- src/DashboardBuild/Components/WeatherComponent/WeatherComponent.tsx
+++ src/DashboardBuild/Components/WeatherComponent/WeatherComponent.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
@@ -76,7 +76,7 @@ export default function WeatherComponent() {
     <div className={styles.container}>
       <div className={styles.moreInfo}>
         <Button variant="primary" onClick={handleShow}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
         </svg>
         </Button>
