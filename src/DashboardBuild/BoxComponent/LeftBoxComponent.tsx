4fcf5f2fb9c3b8f90b3d48123a05911280382a0d
Bruno Cicerchia <brunocicerchia.dev@gmail.com>
2023-10-31 08:48:53 -0300

    Revert "DolarComponent divisa selection added"
    This reverts commit 27f1f22f240c42d377f6a7b3222d08473b52f96c.

--- src/DashboardBuild/BoxComponent/LeftBoxComponent.tsx
+++ src/DashboardBuild/BoxComponent/LeftBoxComponent.tsx
@@ -5,7 +5,9 @@ export default function LeftBoxComponent() {
   return (
     <div className="row mt-md-5 mt-2">
       <div className="col-md-4">
+        <div className={styles.container}>
           <DolarBlueComponent></DolarBlueComponent>
+        </div>
       </div>
       <div className="col-md-4">
         <div className={styles.container}></div>
